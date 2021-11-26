06848b8fe688ca407d089ea779944bb5a76739b0
kayleysyrett <73078305+kayleysyrett@users.noreply.github.com>
2021-11-26 22:52:14 +0000

    Completed wk 6 recording

---
+++ ASP_RazorContoso/Models/Enrollment.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace ASP_RazorContoso.Models
+{
+    public enum Grades
+    {
+        A, B, C, D, F
+    }
+
+    public class Enrollment
+    {
+        public int EnrollmentID { get; set; }
+        public int CourseID { get; set; }
+        public int StudentID { get; set; }
+
+        [DisplayFormat(NullDisplayText = "No grade")]
+        public Nullable<Grades> Grade { get; set; }
+
+        // Navigation Properties
+        public virtual Course Course { get; set; }
+
+        public virtual Student Student { get; set; }
+    }
+}
---
+++ ASP_RazorContoso/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace ASP_RazorContoso.Models
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+
+        // Navigation Property
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
